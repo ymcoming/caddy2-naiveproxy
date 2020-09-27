8dd82f245a318fd8aec21dced2453f1ba07766d6
ymcoming <48167549+ymcoming@users.noreply.github.com>
2020-09-27 16:23:43 +0800

    Update xcaddy.sh

--- src/xcaddy.sh
+++ src/xcaddy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-git clone https://github.com/caddyserver/xcaddy.git && cd xcaddy
-env GOOS=linux GOARCH=amd64 go build -o $PWD/caddy -ldflags "-s -w" ./cmd/xcaddy && chmod +x xcaddy && cd ..
-mv xcaddy/caddy $PWD && rm -rf xcaddy && mv caddy xcaddy
+git clone https://github.com/caddyserver/xcaddy.git && mv xcaddy/* $PWD && rm -rf xcaddy
+env GOOS=linux GOARCH=amd64 go build -o xcaddy -ldflags "-s -w" ./cmd/xcaddy && chmod +x xcaddy
