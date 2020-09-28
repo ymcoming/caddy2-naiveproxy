d738d3ff92f96196078d29ad492bb5d9f5917d6e
ymcoming <48167549+ymcoming@users.noreply.github.com>
2020-09-28 16:12:38 +0800

    Delete xcaddy.sh

--- src/xcaddy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/caddyserver/xcaddy.git && mv xcaddy/* $PWD && rm -rf xcaddy
-env GOOS=linux GOARCH=amd64 go build -o xcaddy -ldflags "-s -w" ./cmd/xcaddy && chmod +x xcaddy
