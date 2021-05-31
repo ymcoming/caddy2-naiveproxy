b0609c5802c25c93823eb8e3085756514705bcb0
ymcoming <48167549+ymcoming@users.noreply.github.com>
2021-05-31 09:30:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /root
-wget https://github.com/ymcoming/caddy2-naiveproxy/releases/download/2.4.1/caddy2  && chmod +x caddy2 && mkdir -m 755 /etc/caddy && mv caddy2 /etc/caddy && wget https://github.com.cnpmjs.org/ymcoming/gost/releases/download/1.0/gost.service && mv ./caddy.service /etc/systemd/system/caddy.service && systemctl daemon-reload && systemctl enable caddy && mkdir -p /var/www/html && cd /var/www/html && 
-wget https://github.com/ymcoming/caddy2-naiveproxy/releases/download/v1.0/27.zip && unzip 27.zip 
+wget https://github.com/ymcoming/caddy2-naiveproxy/releases/download/2.4.1/caddy2  && chmod +x caddy2 && mkdir -m 755 /etc/caddy && mv caddy2 /etc/caddy && wget https://cdn.jsdelivr.net/gh/ymcoming/caddy2-naiveproxy@2.4.2/caddy.service && mv ./caddy.service /etc/systemd/system/caddy.service && systemctl daemon-reload && systemctl enable caddy && mkdir -p /var/www/html && cd /var/www/html && wget https://github.com/ymcoming/caddy2-naiveproxy/releases/download/v1.0/27.zip && unzip 27.zip 
